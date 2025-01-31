6323cce6cec1a098d7e1f93635bea7753f1855e9
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-31 16:30:25 +0330

    create comments module & dev comment entity model

---
+++ src/modules/comments/models/Comment.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  replyTo: {
+    type: mongoose.Types.ObjectId,
+    ref: "Comment",
+  },
+  replays: {
+    type: [mongoose.Types.ObjectId],
+    ref: "Comment",
+    required: true,
+  },
+});
+
+const CommentModel = mongoose.models.Comment ?? mongoose.model("Comment", commentSchema);
+
+export default CommentModel;
